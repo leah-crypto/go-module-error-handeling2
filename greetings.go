65bf877ad2de09da78336892046a254fe1e6a299
Leah Roberson <77908412+leah-crypto@users.noreply.github.com>
2023-02-03 21:00:13 -0600

    update

--- greetings.go
+++ greetings.go
@@ -3,14 +3,30 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func Hello(name string) (string, error ){
 	if name == ""{
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)  //one liner var
+	message := fmt.Sprintf(randomFormat(), name)  //one liner var
 	return message, nil
+}
+
+func init(){
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string{
+	formats := []string {
+		"Hi, %v. Welcome!",
+		"Great to see ya %v!",
+		"I see you once agian %v!",
+	}
+
+	return formats[rand.Intn(len(formats))]
 }
 
 // var message string       other way to declare
